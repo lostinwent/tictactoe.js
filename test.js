9acb947b2b3ef16c48ec47dc0ce413c66016cbe8
coolmat <themattemmons@gmail.com>
2013-06-18 16:38:33 -0400

    finalize javascript logic

---
+++ test.js
@@ -0,0 +1,54 @@
+// var board =  [0, 1, 2
+//               3, 4, 5,
+//               6, 7, 8]
+
+var gameWon = function(mark) {
+  result = false;
+  // rows
+  if (board[0] == mark && board[1] == mark && board[2] == mark) {
+    result = true;
+  } else if (board[3] == mark && board[4] == mark && board[5] == mark) {
+    result = true;
+  } else if (board[6] == mark && board[7] == mark && board[8] == mark) {
+    result = true;
+  // columns
+  } else if (board[0] == mark && board[3] == mark && board[6] == mark) {
+    result = true;
+  } else if (board[1] == mark && board[4] == mark && board[7] == mark) {
+    result = true;
+  } else if (board[2] == mark && board[5] == mark && board[8] == mark) {
+    result = true;
+  // diagonal
+  } else if (board[0] == mark && board[4] == mark && board[8] == mark) {
+    result = true;
+  } else if (board[2] == mark && board[4] == mark && board[6] == mark) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+
+
+
+// x winner on column
+var board = ["1", "2", "3", "4", "5", "6", "X", "X", "X"];
+console.log(gameWon("X"));
+
+
+// o winner on column
+// var board3 = ["1", "2", "O", "4", "5", "O", "7", "8", "O"];
+// console.log(gameWon("O"));
+
+
+
+// // x winner on row
+// var board4 = ["1", "2", "3", "4", "5", "6", "X", "X", "X"];
+// console.log(gameWon("X"));
+
+
+
+// // x winner diag
+// var board5 = ["X", "2", "3", "4", "X", "6", "7", "8", "X"];
+// console.log(gameWon("X"));
